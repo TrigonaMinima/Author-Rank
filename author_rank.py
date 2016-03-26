da8c77bdb762579eaf889e8e586f55e6f144d092
beingcooper <sachin.sharma0728@gmail.com>
2016-03-27 03:33:03 +0530

    showstopper

---
+++ author_rank.py
@@ -0,0 +1,20 @@
+from py2neo import authenticate, Graph, Node, Relationship
+from pprint import pprint
+
+graph = Graph("http://localhost:7474/db/data")
+
+
+query = """
+match (n)-[:Published]->(m)-[:BelongsTo]->(c)
+where c.name = {cat} return n.name, sum(m.q_score) as Score
+order by Score DESC LIMIT 1;
+"""
+
+# Current state : Finds the top author in each category
+
+for a in graph.cypher.execute("match (c:Category) return c.name"):
+	print a[0]
+	for b in  graph.cypher.execute(query, cat=a[0]):
+		print b[0],b[1]
+
+
