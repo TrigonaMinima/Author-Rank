87f8496914ead414fef573d2dc95b94011b98e25
beingcooper <sachin.sharma0728@gmail.com>
2016-04-01 19:48:22 +0530

    python3ized - coz shivam likes it that way

--- author_rank.py
+++ author_rank.py
@@ -7,14 +7,14 @@ graph = Graph("http://localhost:7474/db/data")
 query = """
 match (n)-[:Published]->(m)-[:BelongsTo]->(c)
 where c.name = {cat} return n.name, sum(m.q_score) as Score
-order by Score DESC LIMIT 1;
+order by Score DESC LIMIT 5;
 """
 
 # Current state : Finds the top author in each category
 
 for a in graph.cypher.execute("match (c:Category) return c.name"):
-	print a[0]
+	print(a[0])
 	for b in  graph.cypher.execute(query, cat=a[0]):
-		print b[0],b[1]
+		print(b[0],b[1])
 
 
